a1965a323d0b6f808b6c12a34a9e167dce795e4f
@KarinaDVI <kbbouza@gmail.com>
2023-05-17 16:09:48 -0300

    cambio interfaz

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Descargado from './Componentes/Descargado';
+ import DragImgmemes from './Componentes/DragImgmemes';
+import EjemploDrag from './Componentes/EjemploDrag';
+import Peliculas from './Componentes/Peliculas';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* <Descargado/>*/}
+     <DragImgmemes/> 
+      {/*<EjemploDrag/>*/}
+
+      {/* <ImgMemes2/> */}
+
+      {/* <Peliculas/> */}
+
+    </div>
+  );
+}
+
+export default App;
